51beb3fa19460150f85595ee45263df4893e5d38
Akshay N V <akshayvenkatesha@gmail.com>
2017-12-09 12:45:20 +0530

    Created first project

---
+++ src/module1.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akvenkat
+#
+# Created:     09/12/2017
+# Copyright:   (c) akvenkat 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print("hello world")
+    pass
+
+if __name__ == '__main__':
+    main()
